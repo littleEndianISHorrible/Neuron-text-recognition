a7f98c672172f7c72e6144a6bac9a8104eb486d5
littleEndianISHorrible <maxik.chikin@gmail.com>
2024-05-27 17:58:41 +0800

    streamlit web-UI added
    database work with basic script added
    
    Need to finish a backend (recognition.py)

---
+++ src/front/stlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title('document recognition tool')
+st.subheader('you can download your file to recognize text in it, then you will get a dataframe that will be show below')
+uploaded_file = st.file_uploader('choise a *.pdf-file', type = 'pdf') # type = 'pdf' is optional
+if uploaded_file is not None and uploaded_file.name.endswith('.pdf'): # check if file is not none and it is a pdf file.
+	st.write("I've got a file!")
+else:
+	st.write("I've got nothing to working with!") # if file is not a pdf file, show a message
+	
+# add a button to run script src/backend/recognition.py
+if st.button('run'):
+	st.write('running...')
+	
+# add button to download the result of the script into database
