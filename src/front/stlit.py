1c53e8bc282b3f07bc1ee20d3bc26162bf2f51b4
Владимир Москвин <dzhankui@users.noreply.github.com>
2024-08-26 14:29:13 +0300

    comments

--- src/front/stlit.py
+++ src/front/stlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title('document recognition tool')
 st.subheader('you can download your file to recognize text in it, then you will get a dataframe that will be show below')
-uploaded_file = st.file_uploader('choise a *.pdf-file', type = 'pdf') # type = 'pdf' is optional
+uploaded_file = st.file_uploader('choise a *.pdf-file', type = 'pdf') # optional
 if uploaded_file is not None and uploaded_file.name.endswith('.pdf'): # check if file is not none and it is a pdf file.
 	st.write("I've got a file!")
 else:
